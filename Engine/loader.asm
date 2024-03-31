d6316c17b75907c5efe1620c8f2d636d6c6bf85e
bartfuture <bartfuture@gmail.com>
2024-03-31 21:37:56 +0300

    1

--- Engine/loader.asm
+++ Engine/loader.asm
@@ -34,6 +34,7 @@
 
 ;GentleJungleMap01: need 3 months to kill all creatures (i finished in month 3, week 3)
 ;GentleDesertMap02: need 3 months to kill all creatures (i finished in month 3, week 4)
+;GentleWinterMap01: need 2 months to kill all creatures (i finished in month 2, week 4)
 
 
 LenghtMapData:  equ GentleAutumnMap01-GentleMap03
